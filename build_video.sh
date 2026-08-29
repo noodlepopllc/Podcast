3d13bef8423951fee1c9bec9ef8cdee51386181c
Todd Thomas <noodlepopllc@gmail.com>
2026-08-29 16:16:42 -0400

    Improved combining of clips into final output.

--- build_video.sh
+++ build_video.sh
@@ -136,7 +136,7 @@ for wav in "$INPUT_DIR"/${prefix}*.wav; do
 
     uv run speech_to_video \
         -A "$REF" \
-        -T "$text Stop." \
+        -T "$text Propeller triangle." \
         -P "$PROMPT" \
         -I "$ACTOR" \
         -D "$duration" \
